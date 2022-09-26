58c73fce449e5210c18a49d32db8e60a63696e5a
stefan <fanste.osk@gmail.com>
2022-09-26 18:57:41 +0200

    id it's fast and works.. and fast

--- multithread.py
+++ multithread.py
@@ -2,40 +2,46 @@ from threading import Thread
 import time
 from pytube import Playlist, YouTube
 import os
+import traceback
 
-dl_dir = f"D:\Audio\EasyTube"
+dl_dir = "D:\Audio\EasyTube"
+chars = '"\\/|:?*'
 
 def youtube(url):
     yt = YouTube(url)
-    if yt.check_availability() == None:
-        if not os.path.exists(dl_dir):
-            os.makedirs(dl_dir)
-        os.chdir(dl_dir)
-        title = yt.title.replace(".", "")
-        if not os.path.exists(f"{dl_dir}\{title}.mp3"):
-            try:
-                audio = yt.streams.filter(only_audio=True).first()
-                file = audio.download()
-                file_name, ext = os.path.splitext(file)
-                new_file_name = file_name + ".mp3"
-                os.rename(file, new_file_name)
-                print(f"Downloaded {title}")
-            except Exception:
-                print(f"Something went wrong ({title})")
-                os.remove(file)
-        else: print(f"{title}.mp3 already exists")
-    else: print(f"Error")
-        
+    title = yt.title
+    for char in chars:
+        if char in title:
+            title = title.replace(char, "")
+    title += ".mp3"
+    if not os.path.exists(os.path.join(dl_dir, title)):
+        try:
+            audio = yt.streams.filter(only_audio=True).first()
+            audio.download(filename=title)
+            print(f"Downloaded {yt.title}")
+        except Exception:
+            pass
+    else: print(f"{yt.title}.mp3 already exists")
+
 url = input("Enter URL: ")
 
 start_time = time.time()
 
 if "watch" in url:
+    if not os.path.exists(dl_dir):
+        os.makedirs(dl_dir)
+
+    os.chdir(dl_dir)
     youtube(url)
+
 elif "playlist" in url:
     threads = []
     p = Playlist(url)
-    dl_dir = f"D:\Audio\EasyTube\{p.title}"
+    dl_dir = os.path.join(dl_dir, p.title)
+    if not os.path.exists(dl_dir):
+        os.makedirs(dl_dir)
+    os.chdir(dl_dir)
+
     for i in range(len(p)):
         thread = Thread(target=youtube, args=(p[i],))
         threads.append(thread)
